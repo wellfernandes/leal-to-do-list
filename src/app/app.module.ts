ba06252d2358f76044efe03c4f729c32ab13216f
Welliton Fernandes Leal <wellfernandes@me.com>
2023-10-08 20:17:28 -0300

    feat: :sparkles: added home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponentComponent } from './components/home-page-component/home-page-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponentComponent
   ],
   imports: [
     BrowserModule,
