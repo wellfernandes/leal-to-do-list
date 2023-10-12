a453aa2bb09246c64d3f75789104a3b338d6bb9e
Welliton Fernandes Leal <wellfernandes@me.com>
2023-10-12 17:48:43 -0300

    feat: :sparkles: added side bar function and fixed logo image
    added sidebar menu function, added material icons and angular types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
-declare var materiallize: any;
+import * as M from 'materialize-css'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  
-  ngOnInit(): void {
-      materiallize.AutoInit();
+export class AppComponent implements OnInit {
+  ngOnInit(): void {   
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems);
+    });
   }
-  
-  title = 'leal-to-do-list';
+
+  title = 'Leal To Do List';
 }
