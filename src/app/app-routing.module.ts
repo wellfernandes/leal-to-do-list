95fd9050b1f1bc380299171ca92fb93f23fe81f5
Welliton Fernandes Leal <wellfernandes@me.com>
2023-10-16 18:20:06 -0300

    feat: :sparkles: added loading of components via routes
    added loading of components via routes and changed body color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DynamicBackgroundImageComponentComponent } from './components/dynamic-background-image-component/dynamic-background-image-component.component';
+import { RegistrationComponentComponent } from './components/registration-component/registration-component.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'', component: DynamicBackgroundImageComponentComponent},
+                        {path:'registration', component: RegistrationComponentComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/registration-form-component/registration-form-component.component.ts
+++ src/app/components/registration-form-component/registration-form-component.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-form-component',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class RegistrationFormComponentComponent {
 
+  constructor(private router: Router) {}
+
+  taskRegistration() {
+    console.log('Nova Tarefa Cadastrada');
+  }
+
+  taskCancel() {
+    console.log('Cadastro de Tarefa Cancelada');
+    this.router.navigate(['']);
+  }
 }
