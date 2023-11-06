cfd5c7f9cf3259ee0fb8b0f7791c827727f6dd27
Welliton Fernandes Leal <wellfernandes@me.com>
2023-11-05 23:04:13 -0300

    feat: :sparkles: added error constants

---
+++ src/app/constants/errors.constants.ts
@@ -0,0 +1,4 @@
+export const Errors = {
+    INVALID_TASK_NAME_ERROR: 'O nome não pode conter caracteres especiais, a palavra "Tarefa" ou ter somente números.',
+    EMPTY_FIELD_ERROR: 'O Nome não pode ser vazio.',
+};
