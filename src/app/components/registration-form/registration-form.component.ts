c79a786c13b27bc6ff05ba7ae4d77be849f28a1c
Welliton Fernandes Leal <wellfernandes@me.com>
2023-11-05 23:05:41 -0300

    feat: :sparkles: modified component to validate the input and display an error message
    modified component to validate the input and display an error message by changing the registration button

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Input, Output, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Priority } from "../../constants/priority.constants";
-import {AppTexts} from "../../constants/appTexts.constants";
+import { AppTexts } from "../../constants/appTexts.constants";
+import { Errors } from "../../constants/errors.constants";
 
 @Component({
   selector: 'app-registration-form',
@@ -14,6 +15,9 @@ export class RegistrationFormComponent implements AfterViewInit {
   @Input() taskPriority: string = Priority.TASK_PRIORIRY_LOW;
 
   regex = /^(?!.*Tarefa)(?=.*[a-zA-Z])[A-Za-z0-9\s]+$/i;
+  isButtonDisabled: boolean = true;
+  errorMessage: string = '';
+
   constructor(private router: Router) {}
 
   ngAfterViewInit(): void {
@@ -22,30 +26,51 @@ export class RegistrationFormComponent implements AfterViewInit {
     }
 
   taskRegistration() {
-    if (!(this.taskName === '')) {
-      if (this.regex.test(this.taskName) && !this.taskName.includes('Tarefa')) {
-        this.taskAdded.emit({ name: this.taskName, priority: this.taskPriority });
-        return;
-      } else {
-        window.alert(
-            '\nNome inválido. \n\nO nome não pode conter caracteres especiais\n ou a palavra "Tarefa" ou ter somente números.'
-        );
-        this.clearTaskName();
-        return;
+    if (this.isButtonDisabled) {
+      return;
+    }else{
+      if (!(this.taskName === '')) {
+        if (this.regex.test(this.taskName) && !this.taskName.includes('Tarefa')) {
+          this.taskAdded.emit({ name: this.taskName, priority: this.taskPriority });
+          this.errorMessage = '';
+          return;
+        }
       }
+      this.isButtonDisabled = true;
     }
-    window.alert('\nO Nome não pode ser vazio.');
   }
+
   taskCancel() {
-    console.log('\n\nCadastro de Tarefa Cancelada');
     this.clearTaskName();
   }
+
   clearTaskName() {
     this.taskName = '';
+    this.validateInput();
   }
 
   selectPriority(selectedPriority: string) {
     this.taskPriority = selectedPriority;
+    this.validateInput();
+  }
+
+  validateInput() {
+    if (this.taskName === '') {
+      this.isButtonDisabled = true;
+      this.errorMessage = Errors.EMPTY_FIELD_ERROR;
+    } else {
+      if (this.regex.test(this.taskName) && !this.taskName.includes('Tarefa')) {
+        this.isButtonDisabled = false;
+        this.errorMessage = '';
+      } else {
+        this.isButtonDisabled = true;
+        this.errorMessage = Errors.INVALID_TASK_NAME_ERROR;
+      }
+    }
+  }
+
+  onInputChange() {
+    this.validateInput();
   }
 
   protected readonly Priority = Priority;
