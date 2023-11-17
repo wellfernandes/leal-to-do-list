1e88e6c3427f1264dd195b74661f29722fdc5301
Welliton Fernandes Leal <wellfernandes@me.com>
2023-11-16 21:32:35 -0300

    fix: :adhesive_bandage: fixed title of new task registration page

--- src/app/constants/appTexts.constants.ts
+++ src/app/constants/appTexts.constants.ts
@@ -1,6 +1,6 @@
 export const AppTexts = {
     APP_NAME: 'Leal To Do List',
-    REGISTRATION_PAGE_TITLE: 'Cadastrar de Tarefa',
+    REGISTRATION_PAGE_TITLE: 'Cadastrar Tarefa',
     LISTING_PAGE_TITLE: 'Lista de Tarefas',
     EDIT_PAGE_TITLE: 'Editar Tarefa',
     PROHIBITED_WORD: 'Tarefa',
